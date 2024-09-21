a8dc74283d3f46130832999effd88fac6ad33a96
Anderson Lourenço <68036013+Derson-Lourenco@users.noreply.github.com>
2024-09-20 21:39:05 -0300

    add

--- routes/contratos.js
+++ routes/contratos.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // Receba a conexão MySQL como argumento
-module.exports = (connection) => {
+module.exports = connection => {
   // Rota para obter detalhes dos contratos
   router.get('/getContratos', (req, res) => {
     connection.query('SELECT * FROM contratos', (err, results) => {
       if (err) {
-        console.error('Erro ao obter contratos:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' });
-        return;
+        console.error('Erro ao obter contratos:', err)
+        res.status(500).json({ error: 'Erro interno do servidor' })
+        return
       }
-      res.status(200).json({ success: true, contratos: results });
-    });
-  });
+      res.status(200).json({ success: true, contratos: results })
+    })
+  })
 
   // Rota para salvar um novo contrato
   router.post('/salvarContrato', (req, res) => {
@@ -29,8 +29,8 @@ module.exports = (connection) => {
       dataInicio,
       dataFinalizacao,
       objetoContrato,
-      secretarias,
-    } = req.body;
+      secretarias
+    } = req.body
 
     const sql = `
       INSERT INTO contratos (
@@ -47,7 +47,7 @@ module.exports = (connection) => {
         objetoContrato,
         secretarias
       ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+    `
 
     const values = [
       processoAno,
@@ -61,41 +61,45 @@ module.exports = (connection) => {
       dataInicio,
       dataFinalizacao,
       objetoContrato,
-      secretarias,
-    ];
+      secretarias
+    ]
 
     connection.query(sql, values, (err, result) => {
       if (err) {
-        console.error('Erro ao salvar contrato:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' });
-        return;
+        console.error('Erro ao salvar contrato:', err)
+        res.status(500).json({ error: 'Erro interno do servidor' })
+        return
       }
-      res.status(201).json({ success: true, contratoId: result.insertId });
-    });
-  });
+      res.status(201).json({ success: true, contratoId: result.insertId })
+    })
+  })
 
   // Rota para excluir um contrato
   router.delete('/excluirContrato/:id', (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
 
-    connection.query('DELETE FROM contratos WHERE id = ?', [id], (err, result) => {
-      if (err) {
-        console.error('Erro ao excluir contrato:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' });
-        return;
-      }
+    connection.query(
+      'DELETE FROM contratos WHERE id = ?',
+      [id],
+      (err, result) => {
+        if (err) {
+          console.error('Erro ao excluir contrato:', err)
+          res.status(500).json({ error: 'Erro interno do servidor' })
+          return
+        }
 
-      if (result.affectedRows > 0) {
-        res.status(200).json({ success: true });
-      } else {
-        res.status(404).json({ error: 'Contrato não encontrado' });
+        if (result.affectedRows > 0) {
+          res.status(200).json({ success: true })
+        } else {
+          res.status(404).json({ error: 'Contrato não encontrado' })
+        }
       }
-    });
-  });
+    )
+  })
 
   // Rota para atualizar um contrato
   router.put('/atualizarContrato/:id', (req, res) => {
-    const id = req.params.id;
+    const id = req.params.id
     const {
       processoAno,
       numeroContrato,
@@ -108,8 +112,8 @@ module.exports = (connection) => {
       dataInicio,
       dataFinalizacao,
       objetoContrato,
-      secretarias,
-    } = req.body;
+      secretarias
+    } = req.body
 
     const sql = `
       UPDATE contratos
@@ -127,7 +131,7 @@ module.exports = (connection) => {
         objetoContrato = ?,
         secretarias = ?
       WHERE id = ?
-    `;
+    `
 
     const values = [
       processoAno,
@@ -143,42 +147,45 @@ module.exports = (connection) => {
       objetoContrato,
       secretarias,
       id
-    ];
+    ]
 
     connection.query(sql, values, (err, result) => {
       if (err) {
-        console.error('Erro ao atualizar contrato:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' });
-        return;
+        console.error('Erro ao atualizar contrato:', err)
+        res.status(500).json({ error: 'Erro interno do servidor' })
+        return
       }
 
       if (result.affectedRows > 0) {
-        res.status(200).json({ success: true });
+        res.status(200).json({ success: true })
       } else {
-        res.status(404).json({ error: 'Contrato não encontrado' });
+        res.status(404).json({ error: 'Contrato não encontrado' })
       }
-    });
-  });
+    })
+  })
 
   // Rota para obter detalhes de um contrato específico
-  router.get('/getContratos/:id', (req, res) => {
-    const { id } = req.params;
+  router.get('/contratos/:id', (req, res) => {
+    const { id } = req.params
 
-    connection.query('SELECT * FROM contratos WHERE id = ?', [id], (err, result) => {
-      if (err) {
-        console.error('Erro ao buscar contrato:', err);
-        res.status(500).json({ error: 'Erro interno do servidor' });
-        return;
+    connection.query(
+      'SELECT * FROM contratos WHERE id = ?',
+      [id],
+      (err, result) => {
+        if (err) {
+          console.error('Erro ao buscar contrato:', err)
+          res.status(500).json({ error: 'Erro interno do servidor' })
+          return
+        }
+
+        if (result.length === 0) {
+          res.status(404).json({ error: 'Contrato não encontrado' })
+        } else {
+          res.status(200).json({ success: true, contrato: result[0] })
+        }
       }
+    )
+  })
 
-      if (result.length === 0) {
-        res.status(404).json({ error: 'Contrato não encontrado' });
-      } else {
-        res.status(200).json({ success: true, contrato: result[0] });
-      }
-    });
-  });
-
-
-  return router;
-};
+  return router
+}
