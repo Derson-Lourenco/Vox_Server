35ea9af25b7dc2519ac40cb8b4a4924a63b211ed
Anderson Lourenço <68036013+Derson-Lourenco@users.noreply.github.com>
2024-10-04 12:02:19 -0300

    add

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+const dotenv = require('dotenv')
 
-dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
+dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
 // Configurações de conexão MySQL
 const connection = mysql.createConnection({
@@ -14,15 +14,15 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER || 'dersonls',
   password: process.env.DB_PASSWORD || 'Lara795816',
   database: process.env.DB_NAME || 'gerenciador'
-});
+})
 
-connection.connect((err) => {
+connection.connect(err => {
   if (err) {
-    console.error('Erro ao conectar ao MySQL:', err);
-    return;
+    console.error('Erro ao conectar ao MySQL:', err)
+    return
   }
-  console.log('Conectado ao banco de dados MySQL com sucesso!');
-});
+  console.log('Conectado ao banco de dados MySQL com sucesso!')
+})
 
 // Configuração do CORS
 const corsOptions = {
@@ -32,30 +32,30 @@ const corsOptions = {
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
-};
-app.use(cors(corsOptions));
-app.use(express.json());
+}
+app.use(cors(corsOptions))
+app.use(express.json())
 
 // Middleware para registrar solicitações
 app.use((req, res, next) => {
-  console.log(`Recebida solicitação: ${req.method} ${req.url}`);
-  next();
-});
+  console.log(`Recebida solicitação: ${req.method} ${req.url}`)
+  next()
+})
 
-// Importa e usa as rotas
-const clientesRouter = require('./routes/clientes')(connection);
-app.use('/clientes', clientesRouter);
+// Importa e usa as rotas para paginas 
+const clientesRouter = require('./routes/clientes')(connection)
+app.use('/clientes', clientesRouter)
 
-const loginRouter = require('./routes/login')(connection);
-app.use('/', loginRouter); // Atualizando a rota de login
+const loginRouter = require('./routes/login')(connection)
+app.use('/login', loginRouter) // Atualizando a rota de login
 
-const contratosRouter = require('./routes/contratos')(connection);
-app.use('/contratos', contratosRouter);
+const contratosRouter = require('./routes/contratos')(connection)
+app.use('/contratos', contratosRouter)
 
-const licitacoesRouter = require('./routes/licitacoes')(connection);
-app.use('/licitacoes', licitacoesRouter);
+const licitacoesRouter = require('./routes/licitacoes')(connection)
+app.use('/licitacoes', licitacoesRouter)
 
 // Inicia o servidor
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
