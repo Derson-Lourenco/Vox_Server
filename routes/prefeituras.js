21cd47eabd8f2d96f8aab18f37cae48f13609fc7
Anderson Lourenço <68036013+Derson-Lourenco@users.noreply.github.com>
2024-10-04 15:40:27 -0300

    add

--- routes/prefeituras.js
+++ routes/prefeituras.js
@@ -6,28 +6,33 @@ const router = express.Router();
 // Rota para obter a lista de municípios
 router.get('/', async (req, res) => {
   try {
-    // 1. Fazer a requisição para a API de municípios
+    // Fazer a requisição para a API de municípios
     console.log('Fazendo requisição para a API de Municípios...');
     const response = await axios.get('http://sistemas.tce.pi.gov.br/api/portaldacidadania//prefeituras');
     console.log('Resposta da API de Municípios:', response.data); // Para verificar a resposta
 
     const municipios = response.data; // Obter a lista de municípios
 
-    // 2. Verifique se os dados estão disponíveis
+    // Verifique se os dados estão disponíveis
     if (!municipios || !Array.isArray(municipios.prefeituras)) {
       console.error('Dados de municípios não disponíveis:', municipios);
       return res.status(500).json({ message: 'Dados de municípios não disponíveis' });
     }
 
-    // 3. Filtrar e mapear apenas o ID e o nome
+    // Filtrar e mapear apenas o ID e o nome, e incluir outros campos se necessário
     const resultado = municipios.prefeituras.map(municipio => ({
       id: municipio.id,
-      nome: municipio.nome
+      nome: municipio.nome,
+      codIBGE: municipio.codIBGE,
+      idMunicipioMapa: municipio.idMunicipioMapa,
+      ods: municipio.ods,
+      urlPrefeitura: municipio.urlPrefeitura,
+      urlCamara: municipio.urlCamara
     }));
 
     console.log('Resultado:', resultado); // Para visualizar o resultado no console
 
-    // 4. Retornar os dados filtrados
+    // Retornar os dados filtrados
     res.json(resultado);
   } catch (error) {
     console.error('Erro ao buscar municípios:', error.message);
