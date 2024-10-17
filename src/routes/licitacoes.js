a963ae88914cd9b2387c51e296f72d430146d64d
Anderson Lourenço <68036013+Derson-Lourenco@users.noreply.github.com>
2024-10-16 21:45:50 -0300

    res

--- src/routes/licitacoes.js
+++ src/routes/licitacoes.js
@@ -5,35 +5,49 @@ const router = express.Router()
 
 // Lista fixa de IDs predefinidos
 const idsPredefinidos = ['127', '1478'] // IDs fixos para teste
-//dd
+
 module.exports = connection => {
   // Rota para buscar licitações
   router.get('/', async (req, res) => {
     try {
+      console.log('Entrou na rota GET /') // Log indicando que a rota foi acessada
+
       // 1. Obter as licitações para cada ID predefinido
       const licitacoesPromises = idsPredefinidos.map(async idUnidadeGestora => {
+        console.log(`Buscando licitações para ID: ${idUnidadeGestora}`) // Log para cada ID
+
         const licitacoesResponse = await axios.get(
           `https://sistemas.tce.pi.gov.br/api/portaldacidadania/licitacoes/${idUnidadeGestora}`
         )
+
+        console.log(`Licitações recebidas para ID: ${idUnidadeGestora}`, licitacoesResponse.data) // Log das licitações recebidas
         return { idUnidadeGestora, licitacoes: licitacoesResponse.data }
       })
 
       const licitacoesResults = await Promise.all(licitacoesPromises)
 
+      console.log('Resultados de licitações:', licitacoesResults) // Log dos resultados obtidos
+
       // 2. Buscar detalhes para cada licitação
       const detalhesPromises = licitacoesResults.flatMap(
         ({ idUnidadeGestora, licitacoes }) =>
           licitacoes.map(async licitacao => {
+            console.log(`Buscando detalhes para licitação ${licitacao.id} do ID ${idUnidadeGestora}`) // Log para cada detalhe de licitação
+
             const formattedDate = licitacao.data.split('T')[0].replace(/-/g, '') // Formatar data para AAAAMMDD
             const detalhesResponse = await axios.get(
               `https://sistemas.tce.pi.gov.br/api/portaldacidadania/licitacoes/${idUnidadeGestora}/1/${formattedDate}`
             )
+
+            console.log(`Detalhes recebidos para licitação ${licitacao.id}`, detalhesResponse.data) // Log dos detalhes recebidos
             return detalhesResponse.data
           })
       )
 
       const detalhesArray = await Promise.all(detalhesPromises)
       const detalhesFlattened = detalhesArray.flat()
+
+      console.log('Detalhes combinados:', detalhesFlattened) // Log dos detalhes finais
 
       // Retornar os dados combinados
       res.json(detalhesFlattened)
