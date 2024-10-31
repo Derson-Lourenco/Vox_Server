ab3721f0a9de648900727dfe3003bdefeb4172bd
Anderson Lourenço <68036013+Derson-Lourenco@users.noreply.github.com>
2024-10-31 20:47:57 -0300

    add

--- src/routes/municipios.js
+++ src/routes/municipios.js
@@ -8,12 +8,18 @@ const createMunicipiosRouter = (connection) => {
   router.get('/', async (req, res) => {
     try {
       const response = await axios.get('http://sistemas.tce.pi.gov.br/api/portaldacidadania//prefeituras');
-      const municipios = response.data.prefeituras;
+      
+      // Log da resposta da API externa
+      console.log('Resposta da API externa:', response.data);
+      
+      const municipios = response.data; // Ajuste conforme necessário, dependendo da estrutura da resposta
 
+      // Verifica se os dados estão disponíveis e são uma array
       if (!municipios || !Array.isArray(municipios)) {
         return res.status(500).json({ message: 'Dados de municípios não disponíveis' });
       }
-
+      
+      // Transforma o resultado
       const resultado = municipios.map(municipio => ({
         id: municipio.id,
         nome: municipio.nome,
@@ -23,27 +29,6 @@ const createMunicipiosRouter = (connection) => {
     } catch (error) {
       console.error('Erro ao buscar municípios:', error.message);
       res.status(500).json({ message: 'Erro ao buscar municípios', error: error.message });
-    }
-  });
-
-  // Rota para salvar a relação entre contrato e municípios selecionados
-  router.post('/salvar-prefeituras', async (req, res) => {
-    const { contrato_id, municipios } = req.body;
-
-    if (!contrato_id || !Array.isArray(municipios)) {
-      return res.status(400).json({ message: 'Dados inválidos. Necessário contrato_id e lista de municipios.' });
-    }
-
-    try {
-      // Salvar cada município selecionado com o contrato_id fornecido
-      const values = municipios.map((municipio_id) => [contrato_id, municipio_id]);
-      const query = 'INSERT INTO municipios_PI (contrato_id, municipio_id) VALUES ?';
-      await connection.query(query, [values]);
-
-      res.status(201).json({ message: 'Municípios salvos com sucesso!' });
-    } catch (error) {
-      console.error('Erro ao salvar municípios:', error.message);
-      res.status(500).json({ message: 'Erro ao salvar municípios' });
     }
   });
 
